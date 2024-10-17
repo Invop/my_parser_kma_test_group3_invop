e3608c637ec8ba4080555c45bcb84d3b61e71226
Andrii Shandryk <shandrykjj512@gmail.com>
2024-10-17 15:51:06 +0300

    init

---
+++ Readme.md
@@ -0,0 +1,11 @@
+### My test Parser
+
+my parser for edu purpose
+![img.png](assets/img.png)
+
+### Example 
+```rust
+...
+```
+
+
---
+++ src/main.rs
@@ -0,0 +1,4 @@
+use my_parser_kma_test_group3_invop::*;
+pub fn main() {
+    assert_eq!(list_parser::list("[1,1,2,3,5,8]"), Ok(vec![1, 1, 2, 3, 5, 8]));
+}
